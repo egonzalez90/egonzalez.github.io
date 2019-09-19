2decdba632b549308c29ee16395ec23b904cbbe0
root <root@localhost.localdomain>
2019-09-19 21:22:40 +0200

    Increase window witdh

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,11 @@ html_theme = 'alabaster'
 # documentation.
 #
 # html_theme_options = {}
-
+html_theme_options = {
+    'page_width': 'auto',
+    'body_max_width': 'auto',
+    # 'sidebar_width': 'auto',
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
